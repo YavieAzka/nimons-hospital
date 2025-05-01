b42b256d83154a51be4f93d703a349183e59e9ad
YavieAzka <yavieazkaputra@gmail.com>
2025-05-01 23:04:54 +0700

    feat: added F01.Login feature and apply it to main()

---
+++ src/header/login.h
@@ -0,0 +1,29 @@
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include "user.h"
+
+User login(User *user_now);
+int getUserIndex(const char* username, User* users, int user_count);
+User* getUserData(const char* filename, int* user_count);
+int getTrombosit(char* line);
+int getKadarKolesterolLDL(char* line);
+int getKadarKolesterol(char* line);
+float getTinggiBadan(char* line);
+int getSaturasiOksigen(char* line);
+int getKadarGulaDarah(char* line);
+float getBeratBadan(char* line);
+float getSuhuTubuh(char* line);
+int getTekananDarahSistolik(char* line);
+int getTekananDarahDiastolik(char* line);
+int getDetakJantung(char* line);
+int stringToInt(char* str);
+float stringToFloat(char* str);
+void getUsername(char* line, char* username);
+void getPassword(char* line, char* password);
+void getRole(char* line, char* role);
+void getRiwayatPenyakit(char* line, char* riwayat_penyakit);
+int getId(char* line);
+void getFieldAt(char* line, int field_number, char* output, int max_length);
+
+#endif
