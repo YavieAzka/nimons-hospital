1960785cc3366c437dda587aea182db199c59f2d
YavieAzka <yavieazkaputra@gmail.com>
2025-05-03 12:00:22 +0700

    feat: added Set ADT

---
+++ src/c/set.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "set.h"
+
+void createEmptySet(Set* s) {
+    s->size = 0;
+}
+
+int contains(Set s, const char* str) {
+    for (int i = 0; i < s.size; i++) {
+        if (strcmp(s.elements[i], str) == 0) return 1;
+    }
+    return 0;
+}
+
+void insert(Set* s, const char* str) {
+    if (!contains(*s, str)) {
+        strcpy(s->elements[s->size], str);
+        s->size++;
+    }
+}
