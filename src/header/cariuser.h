a7df0ed7f35e3f8f4b428f36095f728657df0fc0
jerannn24 <junioroppo64@gmail.com>
2025-05-10 21:01:25 +0700

    feat: main, cariuser.h rapihin dikit

--- src/header/cariuser.h
+++ src/header/cariuser.h
@@ -2,26 +2,10 @@
 #include <string.h>
 #include "login.h"
 
-void printPilihan(){
-    printf("Cari berdasarkan:\n");
-    printf("1. ID");
-    printf("2. Nama");
-}
+void printPilihan();
 
+void cariPasien();
 
-void cariPasien(){
+void cariDokter();
 
-}
-
-void cariDokter(){
-
-}
-
-void cariUser(){
-    printPilihan();
-    int userChoice;
-    scanf("%d", &userChoice);
-    if(userChoice == 1){
-
-    }
-}
+void cariUser();
