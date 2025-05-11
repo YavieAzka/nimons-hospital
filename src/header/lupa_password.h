fc316afac9f1fa30167c395737926942e3af6b20
R27-04 <raynaldismb@gmail.com>
2025-05-11 11:18:57 +0700

    feat/lupapassword
    updating typo

--- src/header/lupa_password.h
+++ src/header/lupa_password.h
@@ -7,6 +7,6 @@
 char* generateUniqueCode(const char* username);
 boolean validasiCode(const char* username, const char* inputCode);
 void overwriteUserFile(User* users, int count, const char* filename);
-void gantiPassword(const char* username, const char* newPassword);
+void gantiPassword(User* users, int userCount, const char* username, const char* newPassword);
 void lupaPassword();
 #endif
