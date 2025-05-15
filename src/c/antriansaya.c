4da0e68648dda7192f98023889b1d6b093db8ac2
jerannn24 <junioroppo64@gmail.com>
2025-05-15 20:39:38 +0700

    test

--- src/c/antriansaya.c
+++ src/c/antriansaya.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <strings.h> 
-#include "antriansaya.h"
+#include "../header/antriansaya.h"
 
 Pasien antrian[MAX_PASIEN]; //array buat simpan daftar pasien
 int jumlahPasien = 0;
