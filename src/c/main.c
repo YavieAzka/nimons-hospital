21f4253254c9c6eb274cc471659c1275a8a8010a
YavieAzka <yavieazkaputra@gmail.com>
2025-05-20 11:07:36 +0700

    fix: bug in parsing config.txt

--- src/c/main.c
+++ src/c/main.c
@@ -192,8 +192,22 @@ int main(int argc, char* argv[]) {
         }
     }
     printf("Sampai jumpa di lain hari!\n");
+    printf("%d %d\n", panjang_denah, lebar_denah);
+    for(int i = 0; i < panjang_denah; i++){
+        for(int j = 0; j < lebar_denah; j++){
+            printf("%d\n", ruanganList[i][j].idDokter);
+        }
+    }
     return 0;
 }
 
 // gcc src/c/main.c src/c/login.c src/c/load.c src/c/help.c src/c/register.c src/c/cariuser.c src/c/logout.c src/c/utils.c src/c/set.c src/c/lihatUser.c src/c/queue.c src/c/lupa_password.c -o main
 
+/*
+Applied module:
+F01 - LOGIN
+F02 - REGISTER
+F03 - LOGOUT
+F05 - Menu & Help
+F18 - EXIT
+*/
