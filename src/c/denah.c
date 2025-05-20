60f85ab68fcf5eb2809e6fe959b815071ac46aa4
jerannn24 <junioroppo64@gmail.com>
2025-05-20 21:15:42 +0700

    feat: new feat Denah Dinamic

--- src/c/denah.c
+++ src/c/denah.c
@@ -13,6 +13,9 @@
 #define MAX_LINE_LENGTH 1024
 #define MAX_FIELD_LENGTH 256
 
+int panjang_denah_eff = 0;
+int lebar_denah_eff = 0;
+
 void lihatRuang(char * str){
     int x, y;
     x = str[0] - 'A';
@@ -45,16 +48,16 @@ void lihatRuang(char * str){
 
 void gambarDenah(){
     printf("     ");
-    for (int k = 1; k <= panjang_denah; k++)
+    for (int k = 1; k <= lebar_denah; k++)
     {
         printf("  %d   ", k);
     }
     printf("\n");
 
-    for (int i = 1; i <= lebar_denah; i++)
+    for (int i = 1; i <= panjang_denah; i++)
     {   
         printf("    ");
-        for (int k = 1; k <= panjang_denah; k++)
+        for (int k = 1; k <= lebar_denah; k++)
         {
             printf("+-----");
         }
@@ -62,7 +65,7 @@ void gambarDenah(){
 
         printf(" %c  ", 'A' + i - 1);
 
-        for (int j = 1; j <= panjang_denah; j++)
+        for (int j = 1; j <= lebar_denah; j++)
         {
             printf("| %c%d  ",'A' + i - 1 ,j);
         }
@@ -70,7 +73,7 @@ void gambarDenah(){
     }
 
     printf("    ");
-    for (int k = 1; k <= panjang_denah; k++)
+    for (int k = 1; k <= lebar_denah; k++)
     {
         printf("+-----");
     }
@@ -90,9 +93,56 @@ void denahRumahSakit(char* str){
     }
     else if(strcmp(str, "LIHAT_SEMUA_ANTRIAN") == 0){
         lihatSemuaAntrian();
+    } else if (strcmp(str, "UBAH_DENAH") == 0)
+    {
+        ubahDenah();
+    }
+    
+}
+
+void ubahDenah(){
+    //search eff ukuran denah
+    for (int i = 0; i < panjang_denah; i++) {
+        for (int j = 0; j < lebar_denah; j++) {
+            if (ruanganList[i][j].idDokter != 0) {
+                if (i + 1 > panjang_denah_eff) panjang_denah_eff = i + 1;
+                if (j + 1 > lebar_denah_eff) lebar_denah_eff = j + 1;
+            }
+        }
+    }
+
+    int newPanjang, newLebar;
+    scanf("%d %d", &newPanjang, &newLebar);
+    while (newLebar > MAX_RUANGAN || newPanjang > MAX_RUANGAN){
+        printf("Masukkan Nilai Panjang dan Lebar yang tidak melebihi capacity Ruangan (5 5): \n");
+        scanf("%d %d", &newPanjang, &newLebar);
+    }
+    
+    if (newLebar < lebar_denah_eff || newPanjang < panjang_denah_eff) {
+        int baris = -1, kolom = -1;
+        cariRuang(&baris, &kolom, newLebar, newPanjang);
+        printf("Tidak dapat mengubah ukuran denah. Ruangan %c%d masih ditempati oleh Dr. %s. Silakan pindahkan dokter terlebih dahulu.\n",
+               'A' + baris, kolom + 1, ruanganList[baris][kolom].usernameDokter);
+    } else {
+        lebar_denah = newLebar;
+        panjang_denah = newPanjang;
+        printf("Denah rumah sakit berhasil diubah menjadi %d baris dan %d kolom.\n", newPanjang, newLebar);
     }
 }
 
+void cariRuang(int *baris, int *kolom, int newLebar, int newPanjang) {
+    for (int i = 0; i < panjang_denah; i++) {
+        for (int j = 0; j < lebar_denah; j++) {
+            if (i >= newPanjang || j >= newLebar) {
+                if (ruanganList[i][j].idDokter != 0) {
+                    *baris = i;
+                    *kolom = j;
+                    return;
+                }
+            }
+        }
+    }
+}
 
 /*int main(int argc, char* argv[]) {
     if (argc != 2) {
@@ -106,8 +156,13 @@ void denahRumahSakit(char* str){
     printf("Loading...\n");
     load_all_data(folder);
     printf("Data berhasil dimuat dari folder: %s\n", folder);
-
-    denahRumahSakit("LIHAT_SEMUA_ANTRIAN");
+    bool status = true;
+    while (status == true)
+    {
+        char str[100];
+        scanf("%s", str);
+        denahRumahSakit(str);
+    }
     return 0;
 }*/
 
