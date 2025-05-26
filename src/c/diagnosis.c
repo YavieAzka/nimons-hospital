67de58ba6a334388ad732e249b6e921b7a5127a0
jerannn24 <junioroppo64@gmail.com>
2025-05-27 01:17:53 +0700

    mencari bug pada diagnosis

--- src/c/diagnosis.c
+++ src/c/diagnosis.c
@@ -18,96 +18,54 @@ void searchRuangan(User user, Ruangan *ruangan){
             if (ruanganList[i][j].idDokter == user.id)
             {
                 *ruangan = ruanganList[i][j];
-                break;
+                return;
             }
         }
     }
     return;
 }
 bool cekSuhu(User user, Penyakit penyakit){
-    if (user.suhu_tubuh >= penyakit.suhu_tubuh_min && user.suhu_tubuh <= penyakit.suhu_tubuh_max)
-    {
-        return true;
-    }
-    return false;
+    return user.suhu_tubuh >= penyakit.suhu_tubuh_min && user.suhu_tubuh <= penyakit.suhu_tubuh_max;
 }
 
 bool cekTekananSistolik(User user, Penyakit penyakit){
-    if (user.tekanan_darah_sistolik >= penyakit.tekanan_darah_sistolik_min && user.tekanan_darah_sistolik <= penyakit.tekanan_darah_sistolik_max)
-    {
-        return true;
-    }
-    return false;
+    return user.tekanan_darah_sistolik >= penyakit.tekanan_darah_sistolik_min && user.tekanan_darah_sistolik <= penyakit.tekanan_darah_sistolik_max;
 }
 
 bool cekTekananDiastolik(User user, Penyakit penyakit){
-    if (user.tekanan_darah_diastolik >= penyakit.tekanan_darah_diastolik_min && user.tekanan_darah_diastolik <= penyakit.tekanan_darah_diastolik_max)
-    {
-        return true;
-    }
-    return false;
+    return user.tekanan_darah_diastolik >= penyakit.tekanan_darah_diastolik_min && user.tekanan_darah_diastolik <= penyakit.tekanan_darah_diastolik_max;
 }
 
 bool cekDetakJantung(User user, Penyakit penyakit){
-    if (user.detak_jantung >= penyakit.detak_jantung_min && user.detak_jantung <= penyakit.detak_jantung_max)
-    {
-        return true;
-    }
-    return false;
+    return user.detak_jantung >= penyakit.detak_jantung_min && user.detak_jantung <= penyakit.detak_jantung_max;
 }
 
 bool cekSaturasiOksigen(User user, Penyakit penyakit){
-    if (user.saturasi_oksigen >= penyakit.saturasi_oksigen_min && user.saturasi_oksigen <= penyakit.saturasi_oksigen_max)
-    {
-        return true;
-    }
-    return false;
+    return user.saturasi_oksigen >= penyakit.saturasi_oksigen_min && user.saturasi_oksigen <= penyakit.saturasi_oksigen_max;
 }
 
 bool cekKadarGulaDarah(User user, Penyakit penyakit){
-    if (user.kadar_gula_darah >= penyakit.kadar_gula_darah_min && user.kadar_gula_darah <= penyakit.kadar_gula_darah_max)
-    {
-        return true;
-    }
-    return false;
+    return user.kadar_gula_darah >= penyakit.kadar_gula_darah_min && user.kadar_gula_darah <= penyakit.kadar_gula_darah_max;
 }
 
 bool cekBeratBadan(User user, Penyakit penyakit){
-    if (user.berat_badan >= penyakit.berat_badan_min && user.berat_badan <= penyakit.berat_badan_max)
-    {
-        return true;
-    }
-    return false;
+    return user.berat_badan >= penyakit.berat_badan_min && user.berat_badan <= penyakit.berat_badan_max;
 }
 
 bool cekTinggiBadan(User user, Penyakit penyakit){
-    if (user.tinggi_badan >= penyakit.tinggi_badan_min && user.tinggi_badan <= penyakit.tinggi_badan_max)
-    {
-        return true;
-    }
-    return false;
+    return user.tinggi_badan >= penyakit.tinggi_badan_min && user.tinggi_badan <= penyakit.tinggi_badan_max;
 }
 
 bool cekKadarKolesterol(User user, Penyakit penyakit){
-    if (user.kadar_kolesterol >= penyakit.kadar_kolesterol_min && user.kadar_kolesterol <= penyakit.kadar_kolesterol_max)
-    {
-        return true;
-    }
-    return false;
+    return user.kadar_kolesterol >= penyakit.kadar_kolesterol_min && user.kadar_kolesterol <= penyakit.kadar_kolesterol_max;
 }
 
 bool cekTrombosit(User user, Penyakit penyakit){
-    if (user.trombosit >= penyakit.trombosit_min && user.trombosit <= penyakit.trombosit_max)
-    {
-        return true;
-    }
-    return false;
+    return user.trombosit >= penyakit.trombosit_min && user.trombosit <= penyakit.trombosit_max;
 }
 
 char * cekPenyakit(User pasien){
-    int i = 0;
-    
-    while (i < penyakitCount)
+    for (int i = 0; i < penyakitCount; i++)
     {
         if (cekSuhu(pasien, penyakitList[i]) && cekTekananSistolik(pasien, penyakitList[i]) && cekTekananDiastolik(pasien, penyakitList[i]) && cekDetakJantung(pasien, penyakitList[i]) && cekSaturasiOksigen(pasien, penyakitList[i]) && cekKadarGulaDarah(pasien, penyakitList[i]) && cekBeratBadan(pasien, penyakitList[i]) && cekTinggiBadan(pasien, penyakitList[i]) && cekKadarKolesterol(pasien, penyakitList[i]) && cekTrombosit(pasien, penyakitList[i]))
         {
@@ -143,11 +101,11 @@ void diagnosis(User user){
         
             if (strcmp(namaPenyakit, "Sehat") == 0)
             {
-                printf("%s tidak terdiagnosis penyakit apapun!\n", ruangan.antrianPasien.front->usernamePasien);
+                printf("%s tidak terdiagnosis penyakit apapun!\n", pasien->username);
                 
             } else{
-                printf("%s terdiagnosa penyakit %s!\n", ruangan.antrianPasien.front->usernamePasien, namaPenyakit);
                 strcpy(pasien->riwayat_penyakit, namaPenyakit);
+                printf("%s terdiagnosa penyakit %s!\n", pasien->username, pasien->riwayat_penyakit);
             }
 
         } else{
