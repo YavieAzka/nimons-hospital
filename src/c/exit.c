fcea1e2ec0eb18f3aecd0a191cf0436ab52b1e83
kurakuraninja00 <reysha4school@gmail.com>
2025-06-02 20:18:37 +0700

    hapus comment yg gak perlu

--- src/c/exit.c
+++ src/c/exit.c
@@ -31,25 +31,3 @@ void exit_program() {
     printf("Program dihentikan. Semua dokter, pasien, dan manajer telah meninggalkan rumah sakit.\n");
 }
 
-
-
-// #include <stdio.h>
-// #include <stdlib.h>
-// #include <string.h>
-// #include "../header/exit.h"
-// #include "../header/save.h"
-
-// void exit_program() {
-//     char pilihan[10];
-//     printf("Apakah Anda ingin menyimpan data sebelum keluar? (ya/tidak): ");
-//     scanf("%s", pilihan);
-
-//     if (strcmp(pilihan, "ya") == 0) {
-//         char folder_name[100];
-//         printf("Masukkan nama folder untuk menyimpan data: ");
-//         scanf("%s", folder_name);
-//         save_data(folder_name);
-//     }
-
-//     printf("Program dihentikan. Semua dokter, pasien, dan manajer telah meninggalkan rumah sakit.\n");
-// }
